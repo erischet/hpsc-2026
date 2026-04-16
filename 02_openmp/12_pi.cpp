c1752715d3378981587c502f5e3a06dc9147eeac
erikschetsche <erik.schetsche@campus.tu-berlin.de>
2026-04-16 15:37:33 +0900

    12_pi

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,14 +1,23 @@
-//test
+/*
+Orinal output:
+3.142425985001098
+*/
 
 #include <cstdio>
+#include <omp.h>
 
 int main() {
-  int n = 10;
+  omp_set_num_threads(4);
+  int n = 10; //Tested with Measure-Command { ./a.exe }; Takes n > 1000000 to see actual improvements
   double dx = 1. / n;
   double pi = 0;
+  #pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
   }
+
   printf("%17.15f\n",pi);
 }
+
+//3.142425985001098 - good
