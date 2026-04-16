af7616ed6f3333f64d572e304ce8342321b899dc
erikschetsche <erik.schetsche@campus.tu-berlin.de>
2026-04-16 19:59:56 +0900

    second task

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -1,5 +1,5 @@
 /*
-Orinal output:
+Oringinal output:
 41 67 134 100 169 124 78 158 162 64 105 145 81 27 161 91 195 142 27 36 
 27 27 36 41 64 67 78 81 91 100 105 124 134 142 145 158 161 162 169 195 
 */
@@ -7,6 +7,8 @@ Orinal output:
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
+
 
 void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
@@ -26,24 +28,45 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     vec[begin++] = tmp[i];
 }
 
+
 void merge_sort(std::vector<int>& vec, int begin, int end) {
-  if(begin < end) {
+  if(begin < end) { 
     int mid = (begin + end) / 2;
+
+    #pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+
+    #pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+
+    #pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
 
+
 int main() {
+
+ omp_set_num_threads(4);
+
   int n = 20;
   std::vector<int> vec(n);
+  #pragma omp parallel for
   for (int i=0; i<n; i++) {
     vec[i] = rand() % (10 * n);
+  }
+
+  for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
   }
   printf("\n");
+
+  #pragma omp parallel
+  {
+  #pragma omp single
   merge_sort(vec, 0, n-1);
+  }
+  //Ausgabe, kann so bleiben
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
   }
